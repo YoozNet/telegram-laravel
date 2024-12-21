e62d602facdbe38be748a2d9e6509476f81a523d
Mohammad Azad <158297225+AzadDevX@users.noreply.github.com>
2024-12-21 20:54:07 +0330

    .

--- public/telegram/index.php
+++ public/telegram/index.php
@@ -1,1 +1,6 @@
+<?php
 
+include_once("telegram.php");
+include_once("helper.php");
+
+$updates = Telegram::Updates();
--- public/telegram/telegram.php
+++ public/telegram/telegram.php
@@ -1,10 +1,14 @@
 <?php
 
 class Telegram {
+    private $token;
+    public static function setToken($token): void
+    {
+        self::$token = $token;
+    }
     public static function Api($method,$data=[]) : array
     {
-        $BOT_TOKEN = '';
-        $Request = curl_init("https://api.telegram.org/bot".$BOT_TOKEN."/".$method);
+        $Request = curl_init("https://api.telegram.org/bot".self::$token."/".$method);
         curl_setopt($Request,CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($Request,CURLOPT_POST,1);
         curl_setopt($Request,CURLOPT_POSTFIELDS, $data);
