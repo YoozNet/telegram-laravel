30565f7c4bfb4b5b810fcb898b33052b646caca3
Azad <AzadPC1324@outlook.com>
2024-12-22 23:25:23 +0330

    .

--- telegram/index.php
+++ telegram/index.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 include_once("boot.php");
 
 $Updates = new TelegramUpdates();
+file_put_contents("test_updates.json",json_encode($Updates->update,128|256));
 if($Updates->text == "/start") {
     // createUser('193191319313')
     Telegram::api('sendMessage',[
