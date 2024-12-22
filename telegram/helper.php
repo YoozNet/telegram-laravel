c0dea48fc06e01f4c429fdf304abf50975ac02ea
Golden-Source <mail@goldensource.ir>
2024-12-22 19:56:52 +0330

    connect DB TO Laravel.

--- telegram/helper.php
+++ telegram/helper.php
@@ -10,8 +10,24 @@ function userExists($userId): bool
 }
 function createUser($userId): bool
 {
-    return Database::create('users',
-    ['uuid','first_name','last_name','username'],
-        [$userId,'test 1','test 2','testusername']
+    return Database::create('YN_users',
+    ['user_id','referral_id','api_token'],
+        [$userId,generateString(),generateUUID()]
     );
+}
+function generateUUID() {
+    $uuid = sprintf(
+        '%s-%s-%s-%s-%s',
+        bin2hex(random_bytes(4)),
+        bin2hex(random_bytes(2)),
+        bin2hex(random_bytes(2)),
+        bin2hex(random_bytes(2)),
+        bin2hex(random_bytes(6))
+    );
+
+    return $uuid;
+}
+function generateString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return substr(str_shuffle(str_repeat($characters, ceil($length / strlen($characters)))), 0, $length);
 }
