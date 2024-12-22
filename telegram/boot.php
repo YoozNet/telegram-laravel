e40c79c0be44bd772f2549cdf278d4f0f36824f5
Golden-Source <mail@goldensource.ir>
2024-12-22 18:24:21 +0330

    check vendor

--- telegram/boot.php
+++ telegram/boot.php
@@ -2,8 +2,8 @@
 
 include_once("vendor/autoload.php");
 use Dotenv\Dotenv;
-$env = Dotenv::createImmutable("./");
+$env = Dotenv::createImmutable("../../");
 $env->load();
 
-
-Telegram::setToken($_ENV['TELEGRAM_BOT_TOKEN']);
+// Telegram::setToken($_ENV['TELEGRAM_BOT_TOKEN']);
+Telegram::setToken("1060413360:AAGj_JM6bztQ7JipzwNfD2pRy6cFv_eP2tM");
