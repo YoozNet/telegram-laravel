fe3e5c2eadcaf0b46fe9b69404e949c5df4d12a9
Azad <AzadPC1324@outlook.com>
2024-12-22 22:33:37 +0330

    .

--- telegram/boot.php
+++ telegram/boot.php
@@ -1,16 +1,16 @@
 <?php
 
 include_once("vendor/autoload.php");
-/*
 use Dotenv\Dotenv;
 $env = Dotenv::createImmutable("../../");
 $env->load();
 
+/*
 Telegram::setToken($_ENV['TELEGRAM_BOT_TOKEN']);
 */
 
 Telegram::setToken("1060413360:AAGj_JM6bztQ7JipzwNfD2pRy6cFv_eP2tM");
 
-// Database::connect($_ENV['DB_HOST'],$_ENV['DB_USERNAME'],$_ENV['DB_PASSWORD'],$_ENV['DB_DATABASE']);
+Database::connect($_ENV['DB_HOST'],$_ENV['DB_USERNAME'],$_ENV['DB_PASSWORD'],$_ENV['DB_DATABASE']);
 
-Database::connect('127.0.0.1','root','','azad');
+// Database::connect('127.0.0.1','root','','azad');
