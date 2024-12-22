cc526295e2976726f75f974ca4e0430c289d5a00
Golden-Source <mail@goldensource.ir>
2024-12-22 18:02:30 +0330

    add error log

--- telegram/index.php
+++ telegram/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Content-Type: application/json; charset=utf-8');
+ini_set('error_log', '0.txt');
+error_reporting(E_ALL);
+
 include_once("boot.php");
 
 $Updates = Telegram::updates();
