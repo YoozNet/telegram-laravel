e36deb520c28b4203f358192ad8472fc50e821c0
Golden-Source <mail@goldensource.ir>
2024-12-23 20:22:37 +0330

    fix bug

--- telegram/index.php
+++ telegram/index.php
@@ -210,7 +210,7 @@ $api_token
         $userData = getUser($update->cb_data_chatid);
         $referral = $userData['referral_id'];
         $referral_by = Database::select("YN_users", ["id"], "referred_by = ?", [$referral]);
-        $referral_count = count(array_filter($users, function ($user) use ($referral) {
+        $referral_count = count(array_filter($referral_by, function ($user) use ($referral) {
             return $user['referred_by'] === $referral;
         }));
         Telegram::api('editMessageText',[
