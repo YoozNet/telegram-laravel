05864e0037cfdfe39e5d451f993c96d862e99f00
Golden-Source <mail@goldensource.ir>
2024-12-25 03:43:13 +0330

    fix bug

--- telegram/index.php
+++ telegram/index.php
@@ -137,12 +137,13 @@ try {
         $group_id = App\Enum\UserGroupEnum::from($group_id)->getLabel();
         $discount = $userData['discount'];
         $cardNumber = adminCardNumber($chat_id);
-        $cardInfo = splitCardNumber($cardNumber['card_number']) ?? "تنظیم نشده";
+        $cardInfo = $cardNumber['card_number'] != null ? splitCardNumber($cardNumber['card_number']) : "تنظیم نشده";
         Telegram::api('sendMessage',[
             'chat_id' => $chat_id,
             'text' => "
-ℹ️ اطلاعات حساب کاربری:
-جی میل: ".$email."
+ℹ️ اطلاعات حساب کاربری: ".$userData['id']."
+شناسه مشتری : 
+ایمیل: ".$email."
 شماره کارت پیشفرض برای پرداخت: ".$cardInfo."
 گروه کاربری: ".$group_id."
 تخفیف: ".$discount."%
